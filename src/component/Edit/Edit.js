7ddf125ba5eef9f5ae0df27c5ec5b8fc4185485a
anbiyasr <anbiyaonezero01@gmail.com>
2019-11-11 16:15:40 +0700

    adding edit tab

---
+++ src/component/Edit/Edit.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Edit.css'
+
+class Edit extends Component {
+    // state = {
+
+    // }
+    render(){
+        return(
+            <div>
+                <label>Nama 
+                <input 
+                type="text"
+                placeholder="Masukan Nama"
+                 />
+                </label>
+            </div>
+        )
+    }
+    
+
+}
+export default Edit;
---
+++ src/component/Table/Table.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+import Edit from '../Edit/Edit';
+import { useHistory } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import Icon from '@material-ui/core/Icon';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function Table() {
+  let history = useHistory();
+
+  const handleClick = () => {
+      // alert('tombol di klik')
+      history.push("/Edit");
+  }
+  const classes = useStyles();
+  
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Name', field: 'nama' },
+      { title: 'Alamat', field: 'alamat' },
+      { title: 'Tahun Lahir', field: 'tahunLahir', type: 'numeric' },
+      { title: 'Tempat Lahir', field: 'tempatLahir'}
+    ],
+    data: 
+    // localStorage.getItem("data") 
+    // ? 
+    JSON.parse(localStorage.getItem("data"))
+    // : []
+  });
+  console.log(state)
+  
+  return (
+    <div>
+    <Button variant="contained" className={classes.button} onClick={handleClick}>
+        Edit
+    </Button>
+    <MaterialTable
+      title="DATA DIRI"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data]
+              data.push(newData)
+              // window.localStorage.setItem("data",(state.columns[data]))
+              // console.log(`${data}`)
+              // let newData = localStorage.setItem('data', JSON.stringify(this.state.data))
+              setState({ ...state, data })
+              window.localStorage.setItem("data", JSON.stringify(data))
+              //   ))
+                
+            },
+             600);
+          }),
+
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              const data = [...state.data];
+              data.splice(data.indexOf(oldData), 1);
+              setState({ ...state, data });
+              window.localStorage.setItem("data", JSON.stringify(data))
+            }, 600);
+          }),
+      }}
+    />
+    </div>
+  );
+}
